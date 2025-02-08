5f2828bb4917a27bd624ccf6ae07f4bd0553e3e2
candrepa1 <candrepa1@gmail.com>
2025-02-08 16:16:09 -0500

    Initial Commit

---
+++ ohthatssoon/ohthatssoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ohthatssoonApp.swift
+//  ohthatssoon
+//
+//  Created by cam on 8/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ohthatssoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
